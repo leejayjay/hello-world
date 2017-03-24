78ccd43d9e3e6662d7280062da61f22e28aff981
leejayjay <1103137221@gm.kuas.edu.tw>
2017-03-24 14:30:20 +0800

    hello world

---
+++ WebApplication2/Controllers/Default1Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Default1/
+        public ActionResult Index()
+        {
+            ViewBag.Desc = "Hello world";
+            return View();
+        }
+	}
+}
